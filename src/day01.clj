7155dfb4c5018e6e4ccfb8496628e2d3f9b7d2fa
Szabolcs Sulik <sulik.szabolcs@gmail.com>
2021-12-01 19:37:42 +0100

    day01 second part

--- src/day01.clj
+++ src/day01.clj
@@ -1,6 +1,12 @@
 (ns day01
   (:require [clojure.java.io :as io]))
 
+(def data-sample
+  (-> (io/resource "day01sample.txt")
+      io/reader
+      line-seq))
+
+
 (def data-a
   (-> (io/resource "day01a.txt")
       io/reader
@@ -14,3 +20,13 @@
      count
      )
 ;; => 1233
+
+(->> data-a
+     (map #(Long/parseLong %))
+     (partition 3 1)
+     (map #(reduce + %))
+     (partition 2 1)
+     (filter #(< (nth % 0) (nth % 1)))
+     count
+     )
+;; => 1275
