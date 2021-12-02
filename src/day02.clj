00c5d934c71e899bd3c96dcff86aaa963cf976eb
Szabolcs Sulik <sulik.szabolcs@gmail.com>
2021-12-02 17:40:58 +0100

    day02 part two

--- src/day02.clj
+++ src/day02.clj
@@ -24,3 +24,20 @@
      (reduce *)
      )
 ;; => 1947824
+
+(->> data
+     (partition 2)
+     (map (fn [[dir val]] [dir (Long/parseLong val)]))
+     (reduce (fn [res [dir val]]
+               (case dir
+                 "forward" (-> res
+                               (update :hor #(+ % val))
+                               (update :dep #(+ % (* val (:aim res)))))
+                 "down" (update res :aim #(+ % val))
+                 "up" (update res :aim #(- % val))))
+             {:hor 0, :dep 0, :aim 0})
+     vals
+     (take 2)
+     (reduce *)
+     )
+;; => 1813062561
