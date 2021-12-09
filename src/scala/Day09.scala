27deabb890e7cbf5e4730a01521001eb61e2860d
Szabolcs Sulik <sulik.szabolcs@gmail.com>
2021-12-09 17:31:00 +0100

    day09 part two

--- src/scala/Day09.scala
+++ src/scala/Day09.scala
@@ -1,3 +1,4 @@
+import scala.annotation.tailrec
 import scala.io.Source
 
 object Day09 {
@@ -30,8 +31,7 @@ object Day09 {
     grid.getOrElse((x, y + 1), 10) > grid((x, y))
   }
 
-  def partOne = println {
-    val grid = parseGrid(data)
+  def lowPoints(grid: Grid): Set[Pos] = {
     val Xa = grid.keys.map(_._1).max
     val Ya = grid.keys.map(_._2).max
     val horizontalLows =
@@ -46,12 +46,48 @@ object Day09 {
         x <- 0 to Xa
         if isVerticalLow(grid, x, y)
       } yield (x, y)
-    val lows = horizontalLows.toSet.intersect(verticalLows.toSet)
+    horizontalLows.toSet.intersect(verticalLows.toSet)
+  }
+
+  def partOne = println {
+    val grid = parseGrid(data)
+    val lows = lowPoints(grid)
     grid.filter(x => lows(x._1)).map(_._2 + 1).sum
     // => 496
   }
 
+  def basin(grid: Grid, low: Pos): Set[Pos] = {
+    def neighbours(pos: Pos): Set[Pos] = {
+      Set(
+        (pos._1 - 1, pos._2),
+        (pos._1 + 1, pos._2),
+        (pos._1, pos._2 - 1),
+        (pos._1, pos._2 + 1)
+      ).filter(grid.contains)
+    }
+    @tailrec
+    def loop(basin: Set[Pos], candidates: Set[Pos]): Set[Pos] = {
+      if (candidates.isEmpty)
+        basin
+      else {
+        val newBasin = basin union candidates
+        val newCandidates =
+          candidates.flatMap(neighbours).filter(grid(_) < 9) -- newBasin
+        loop(newBasin, newCandidates)
+      }
+    }
+    loop(Set.empty, Set(low))
+  }
+
+  def partTwo = println {
+    val grid = parseGrid(data)
+    val lows = lowPoints(grid)
+    lows.toList.map(basin(grid, _)).map(_.size).sorted.reverse.take(3).product
+    // => 902880
+  }
+
   def main(args: Array[String]) = {
     partOne
+    partTwo
   }
 }
