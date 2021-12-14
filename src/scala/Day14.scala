d9cb17701c7ca04d0b649b79c75761bd8a213beb
Szabolcs Sulik <sulik.szabolcs@gmail.com>
2021-12-14 13:15:03 +0100

    day14 part two

--- src/scala/Day14.scala
+++ src/scala/Day14.scala
@@ -21,42 +21,60 @@ object Day14 {
       step(result, rules)
     }
     formulaValue(newFormula)
-    // =>
+    // => 3284
   }
 
-  type Elem = String
-  type Pair = String
-  type Formula = String
+  type Elem = Char
+  type Pair = (Char, Char)
+  type Formula = Map[Pair, Long]
   type Rules = Map[Pair, Elem]
 
   def parseInput(lines: Seq[String]): (Formula, Rules) =
-    (lines.head, parseRules(lines.drop(2)))
+    (parseFormula(lines.head), parseRules(lines.drop(2)))
 
-  val ruleR = "(\\w+) -> (\\w)".r
+  def parseFormula(line: String): Formula =
+    line.zip(line.tail).map(_ -> 1L).toMap
+
+  val ruleR = "(\\w)(\\w) -> (\\w)".r
   def parseRules(lines: Seq[String]): Rules =
-    lines.map { case ruleR(pair, el) => pair -> el }.toMap
+    lines.map { case ruleR(a, b, el) => (a.head, b.head) -> el.head }.toMap
 
   def step(formula: Formula, rules: Rules): Formula = {
     formula
-      .zip(formula.tail)
-      .map { case (a, b) =>
-        val el = rules(s"$a$b")
-        s"$a$el"
+      .foldLeft(Map.empty) { case (result, (pair @ (a, b), n)) =>
+        val el = rules(pair)
+        result
+          .updatedWith((a, el))(_.map(_ + n).orElse(Some(n)))
+          .updatedWith((el, b))(_.map(_ + n).orElse(Some(n)))
       }
-      .mkString("", "", formula.last.toString)
   }
 
-  def formulaValue(formula: String): Int = {
+  def formulaValue(formula: Formula): Long = {
     val elems = formula
-      .groupBy(identity)
-      .map { case (k, v) => k -> v.length }
+      .foldLeft(Map.empty[Elem, Long]) { case (result, ((a, b), n)) =>
+        result
+          .updatedWith(a)(_.map(_ + n).orElse(Some(n)))
+          .updatedWith(b)(_.map(_ + n).orElse(Some(n)))
+      }
       .toList
       .sortBy(_._2)
-    println(elems)
-    elems.last._2 - elems.head._2
+      .map { case (k, v) => (k, v / 2) }
+    elems.last._2 - elems.head._2 - 1
+  }
+
+  def partTwo = println {
+    val input = data
+    val (formula, rules) = parseInput(input)
+    val steps = 40
+    val newFormula = (0 until steps).foldLeft(formula) { case (result, _) =>
+      step(result, rules)
+    }
+    formulaValue(newFormula)
+    // => 4302675529689
   }
 
   def main(args: Array[String]) = {
     partOne
+    partTwo
   }
 }
