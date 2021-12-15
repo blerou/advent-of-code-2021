7f6aef2e5f307bbd1f1eb86fe86404644d46d7f8
Szabolcs Sulik <sulik.szabolcs@gmail.com>
2021-12-15 16:36:27 +0100

    day15 part two

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ lazy val root = (project in file("."))
   .settings(
     name := "advent-of-code-2021",
 //    scalaVersion := "2.13.7",
-    scalaVersion := "3.0.2",
+    scalaVersion := "3.1.0",
     Compile / scalaSource := baseDirectory.value / "src" / "scala",
     Compile / resourceDirectory := baseDirectory.value / "resources"
   )
--- src/scala/Day15.scala
+++ src/scala/Day15.scala
@@ -17,47 +17,50 @@ object Day15 {
 
   def partOne = println {
     val input = data
-    val start = Pos(0, 0)
     val grid = parseInput(input)
-    val path =
-      findPath(
-        grid,
-        mutable.PriorityQueue(Path(0, List(start))),
-        Set.empty,
-        grid.keys.max
-      )
+    val path = findPath(grid)
     println(draw(grid, path))
     path.risk
     // => 621
   }
 
-  @tailrec
-  def findPath(
-      grid: Grid,
-      paths: mutable.PriorityQueue[Path],
-      reached: Set[Pos],
-      endPos: Pos
-  ): Path = {
-    val bestPath = paths.dequeue()
-    if (bestPath.path.head == endPos) {
-      bestPath
-    } else {
-      val candidates = possibleStepsForward(bestPath)
-        .filter(grid.contains)
-        .filterNot(reached.contains)
-      candidates
-        .foreach { pos =>
-          val risk = grid(pos)
-          paths.enqueue(Path(bestPath.risk + risk, pos +: bestPath.path))
-        }
-      val newReached = reached ++ candidates.toSet
-      findPath(grid, paths, newReached, endPos)
+  def findPath(grid: Grid): Path = {
+    val startPos = Pos(0, 0)
+    val endPos = grid.keys.max
+    @tailrec
+    def pathFinder(
+        paths: mutable.PriorityQueue[Path],
+        reached: Set[Pos]
+    ): Path = {
+      val bestPath = paths.dequeue()
+      if (bestPath.path.head == endPos) {
+        bestPath
+      } else {
+        val candidates = possibleStepsForward(bestPath)
+          .filter(grid.contains)
+          .filterNot(reached.contains)
+        candidates
+          .foreach { pos =>
+            val risk = grid(pos)
+            paths.enqueue(Path(bestPath.risk + risk, pos +: bestPath.path))
+          }
+        val newReached = reached ++ candidates.toSet
+        pathFinder(paths, newReached)
+      }
     }
-  }
-
-  def possibleStepsForward(path: Path): Seq[Pos] = {
-    val pos = path.path.head
-    List(Pos(pos.x - 1, pos.y), Pos(pos.x + 1, pos.y), Pos(pos.x, pos.y + 1))
+    def possibleStepsForward(path: Path): Seq[Pos] = {
+      val pos = path.path.head
+      List(
+        Pos(pos.x - 1, pos.y),
+        Pos(pos.x + 1, pos.y),
+        Pos(pos.x, pos.y - 1),
+        Pos(pos.x, pos.y + 1)
+      )
+    }
+    pathFinder(
+      mutable.PriorityQueue(Path(0, List(startPos))),
+      Set.empty
+    )
   }
 
   case class Pos(x: Int, y: Int)
@@ -89,13 +92,42 @@ object Day15 {
         (0 to grid.keys.map(_._1).max)
           .map { x =>
             val pos = Pos(x, y)
-            if (path.path.contains(pos)) "." else grid(pos).toString
+            if (path.path.contains(pos)) s"${grid(pos)}\u0300"
+            else grid(pos).toString
           }
           .mkString("")
       }
       .mkString("", "\n", "\n")
 
+  def partTwo = println {
+    val input = data
+    val grid = expand(parseInput(input))
+    val path = findPath(grid)
+//    println(draw(grid, path))
+    path.risk
+    // => 2904
+  }
+
+  def expand(grid: Grid): Grid = {
+    def updatedRisk(risk: Risk, d: Int): Risk = {
+      val newRisk = risk + d
+      if (newRisk > 9) newRisk - 9 else newRisk
+    }
+    val endPos = grid.keys.max
+    val verticalExpand = (1 to 4).foldLeft(grid) { case (result, d) =>
+      result ++ grid.map { case (p, r) =>
+        (Pos(p.x, p.y + (endPos.y + 1) * d), updatedRisk(r, d))
+      }
+    }
+    (1 to 4).foldLeft(verticalExpand) { case (result, d) =>
+      result ++ verticalExpand.map { case (p, r) =>
+        (Pos(p.x + (endPos.x + 1) * d, p.y), updatedRisk(r, d))
+      }
+    }
+  }
+
   def main(args: Array[String]) = {
     partOne
+    partTwo
   }
 }
