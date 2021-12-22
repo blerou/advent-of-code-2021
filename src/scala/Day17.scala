e6ac27440caf5406ba2b2d305ed32bd61f0eb410
Szabolcs Sulik <sulik.szabolcs@gmail.com>
2021-12-23 00:47:16 +0100

    day17 part two

--- src/scala/Day17.scala
+++ src/scala/Day17.scala
@@ -26,7 +26,59 @@ object Day17 {
     }
   }
 
+  def partTwo = println {
+    val input = data
+    val (x1, x2, y1, y2) = parseInput(input)
+    val maxSteps = (math.abs(y1) max math.abs(y2)) * 2
+    // assumes y1 < y2 < 0
+    val yCandidates = y1 until -y1
+    possibleDxWithSteps(x1, x2).toList.sorted
+      .flatMap { case (dx, steps) =>
+        yCandidates
+          .filter { dy =>
+            if (dx > steps) {
+              val sum = LazyList.iterate(dy)(_ - 1).take(steps).sum
+              y1 <= sum && sum <= y2
+            } else {
+              LazyList
+                .iterate(dy)(_ - 1)
+                .take(maxSteps)
+                .inits
+                .filter(_.size >= steps)
+                .exists { s =>
+                  val sum = s.sum
+                  y1 <= sum && sum <= y2
+                }
+            }
+          }
+          .map(dy => (dx, dy))
+      }
+      .toSet
+      .toList
+      .sorted
+      .size
+    // => 4556
+  }
+
+  def possibleDxWithSteps(x1: Int, x2: Int) = {
+    (1 to (x1 max x2))
+      .map { dx =>
+        Range
+          .inclusive(dx, 0, -1)
+          .inits
+          .filter { s =>
+            val sum = s.sum
+            x1 <= sum && sum <= x2
+          }
+          .map(s => (s.head, s.size))
+      }
+      .filter(_.nonEmpty)
+      .map(_.toSet)
+      .reduce(_ union _)
+  }
+
   def main(args: Array[String]) = {
     partOne
+    partTwo
   }
 }
